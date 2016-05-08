bd5eff439def9b2f423f6674b3f0c937090fee30
Mateusz7410 <Mateusz7410@gmail.com>
2016-05-08 21:06:10 +0200

    wykresiki wszystko ładnie

--- BFS3.py
+++ BFS3.py
@@ -5,85 +5,85 @@ import macierz
 import time
 
 def BFS_sasiedztwa(n,tab,bfs):
-    pom=[]
-    for i in range(n):
-        pom.append(0)
+    pom=[0]*n
     for i in range(n):
         for j in range(n):
             if tab[j][i]==1:
                 pom[i]+=1
-    tmp=n
-    while tmp!=0:
-        for i in range(n-1,-1,-1):
-            if pom[i]==0:
-                pom[i]=-1
-                bfs.append(i)
-                tmp-=1
-                for j in range(n):
-                    if tab[i][j]==1:
-                        pom[j]-=1
+    q=[]
+    for i in range(n):
+        if pom[i]==0:
+            q.append(i)
+    while q:
+        tmp=q[0]
+        bfs.append(q.pop(0))
+        for i in range(n):
+            if tab[tmp][i]==1:
+                pom[i]-=1
+                if pom[i]==0:
+                    q.append(i)
+
 
 def BFS_nastepniki(n,nast,bfs):
-    pom = []
-    for i in range(n):
-        pom.append(0)
+    pom=[0]*n
     for i in range(n):
         for j in range(len(nast[i])):
             pom[nast[i][j]]+=1
-    tmp=n
-    while tmp != 0:
-        for i in range(n - 1, -1, -1):
-            if pom[i] == 0:
-                pom[i] = -1
-                bfs.append(i)
-                tmp -= 1
-                for j in range(len(nast[i])):
-                    pom[nast[i][j]]-=1
+    q = []
+    for i in range(n):
+        if pom[i] == 0:
+            q.append(i)
+    while q:
+        tmp=q[0]
+        bfs.append(q.pop(0))
+        for i in range(len(nast[tmp])):
+            pom[nast[tmp][i]]-=1
+            if pom[nast[tmp][i]]==0:
+                q.append(nast[tmp][i])
 
 def BFS_krawedzi(n,kraw,bfs):
-    pom = []
-    for i in range(n):
-        pom.append(0)
+    pom=[0]*n
     for i in range(len(kraw)):
         pom[kraw[i][1]]+=1
-    tmp = n
-    while tmp != 0:
-        for i in range(n-1,-1,-1):
-            if pom[i] == 0:
-                pom[i] = -1
-                bfs.append(i)
-                tmp -= 1
-                for j in range(len(kraw)):
-                    if kraw[j][0]==i:
-                        pom[kraw[j][1]]-=1
+    q = []
+    for i in range(n):
+        if pom[i] == 0:
+            q.append(i)
+    while q:
+        tmp = q[0]
+        bfs.append(q.pop(0))
+        for i in range(len(kraw)):
+            if kraw[i][0]==tmp:
+                pom[kraw[i][1]]-=1
+                if pom[kraw[i][1]]==0:
+                    q.append(kraw[i][1])
 
 def BFS_macierz(n,macie,bfs,npb):
-    pom = []
+    pom=[0]*n
     for i in range(n):
-        pom.append(0)
+        if npb[i][1] != (-1):
+            pom[i] += 1
+            j = npb[i][1]
+            while -macie[i][j] != j:
+                j = -macie[i][j]
+                pom[i] += 1
+    q = []
     for i in range(n):
-        if npb[i][0] != (-1):
-            pom[npb[i][0]] += 1
-            j = npb[i][0]
-            while macie[i][j] != j:
-                j = macie[i][j]
-                pom[j] += 1
-    #print(pom)
-    print(macie)
-    print(npb)
-    tmp = 0
-    while tmp != n:
-        for i in range(n):
-            if pom[i] == 0:
-                pom[i] = -1
-                bfs.append(i)
-                tmp += 1
-                if npb[i][0]!=(-1):
-                    pom[npb[i][0]]-=1
-                    j=npb[i][0]
-                    while macie[i][j]!=j:
-                        j=macie[i][j]
-                        pom[j]-=1
+        if pom[i] == 0:
+            q.append(i)
+    while q:
+        tmp = q[0]
+        bfs.append(q.pop(0))
+        if npb[tmp][0]!=(-1):
+            j = npb[tmp][0]
+            pom[j]-=1
+            if pom[j]==0:
+                q.append(j)
+            while macie[tmp][j] != j:
+                j = macie[tmp][j]
+                pom[j] -= 1
+                if pom[j]==0:
+                    q.append(j)
 
 if __name__ == "__main__":
     '''n=3000
@@ -141,7 +141,7 @@ if __name__ == "__main__":
 
     print("BFS \n\n\n")
     plik = open("plikBFS.csv", "w")
-    for i in range(500, 3001, 500):
+    for i in range(200, 3001, 200):
         n = i
         tab = []
         generowanie.generate(n, tab)
@@ -151,20 +151,43 @@ if __name__ == "__main__":
         wynik = time.time() - start
         plik.write("bfs_sasiedztwa;{};{}\n".format(i, wynik))
         print(bfs)
-        start = time.time()
+
         bfs = []
         nast = []
         nastepniki.nastepniki(n, tab, nast)
+        start = time.time()
         BFS_nastepniki(n, nast, bfs)
         wynik = time.time() - start
         plik.write("bfs_nastepniki;{};{}\n".format(i, wynik))
         print(bfs)
-        start = time.time()
+
         bfs = []
         kraw = []
         krawedzi.krawedzi(n, tab, kraw)
+        start = time.time()
         BFS_krawedzi(n, kraw, bfs)
         wynik = time.time() - start
         plik.write("bfs_krawedzie;{};{}\n".format(i, wynik))
         print(bfs)
+
+
+        nast = []
+        macierz.nastepniki(n, tab, nast)
+
+        pop = []
+        macierz.poprzedniki(n, tab, pop)
+
+        incy = []
+        macierz.incydencji(n, nast, pop, incy)
+
+        bfs = []
+        macie = []
+        npb = []
+        macierz.macierz(n, nast, pop, incy, macie, npb)
+        start = time.time()
+        BFS_macierz(n, macie, bfs, npb)
+        wynik = time.time() - start
+        plik.write("bfs_macierz;{};{}\n".format(i, wynik))
+        print(bfs)
+
     plik.close()
