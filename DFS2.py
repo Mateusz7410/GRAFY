8449dd483d6601ed5cd319e3c4141ecb5c667492
Root <pankernel@tutanota.com>
2016-05-09 23:24:53 +0200

    DFS rev.2 done, minor updates. sasiedztwa - working krawedzie - working nastepniki - working CSV generated.

--- DFS2.py
+++ DFS2.py
@@ -3,6 +3,8 @@ import krawedzi
 import generowanie
 import macierz
 import time
+import sys
+sys.setrecursionlimit(1000000000)
 
 
 class Stack:
@@ -61,7 +63,7 @@ def DFS_nastepniki(n, nast, start):
 def DFS_k(n, odw, kraw, v1):
     if odw[v1] == False:
         odw[v1] = True
-        for i in range(n):
+        for i in range(len(kraw)):
             if kraw[i][0] == v1 and odw[kraw[i][1]] == False:
                 DFS_k(n, odw, kraw, kraw[i][1])
     print(v1)
@@ -70,7 +72,7 @@ def DFS_k(n, odw, kraw, v1):
 
 def DFS_krawedzie(n, kraw, start):
     v1 = start
-    odwiedzone = [False] * (n-1)
+    odwiedzone = [False] * n
     while False in odwiedzone:
         DFS_k(n, odwiedzone, kraw, v1)
         if False in odwiedzone:
@@ -81,17 +83,53 @@ def DFS_krawedzie(n, kraw, start):
 
 
 if __name__ == "__main__":
-    tab = [[0,1,0,0,1,0,0,0], [0,0,0,0,0,0,1,0], [0,0,0,0,1,1,0,0], [0,0,0,0,0,1,0,1], [0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,1], [0,0,0,0,0,0,0,0]]
-    #tab =[]
-    #n = 1000
-    #generowanie.generate(n, tab)
-
+    '''#tab = [[0,1,0,0,1,0,0,0], [0,0,0,0,0,0,1,0], [0,0,0,0,1,1,0,0], [0,0,0,0,0,1,0,1], [0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,1], [0,0,0,0,0,0,0,0]]
+    fp = open("dfs2.csv", "r")
+    tab = []
+    n = 14
+    generowanie.generate(n, tab)
+    start = time.time()
     DFS_sasiedztwa(len(tab), tab, 0)
+    print(time.time()-start)
     nast = []
+    start = time.time()
     nastepniki.nastepniki(len(tab), tab, nast)
     DFS_nastepniki(len(nast), nast,0)
+    print(time.time()-start)
+    start = time.time()
     kraw = []
-    dfs = []
     krawedzi.krawedzi(len(tab), tab, kraw)
-    DFS_krawedzie(len(kraw), kraw, 0)
-
+    DFS_krawedzie(n, kraw, 0)
+    print(time.time() - start)'''
+    print("DFS \n\n\n")
+    plik = open("dfs2_bezgenerowania.csv", "w")
+    for i in range(200, 3001, 200):
+        n = i
+        start = time.time()
+        tab = []
+        generowanie.generate(n, tab)
+        wynik = time.time() - start
+        plik.write("Generowanie;{};{}\n".format(i, wynik))
+        dfs = []
+        start = time.time()
+        DFS_sasiedztwa(n, tab, 0)
+        wynik = time.time() - start
+        plik.write("dfs_sasiedztwa;{};{}\n".format(i, wynik))
+        print(dfs)
+        dfs = []
+        nast = []
+        nastepniki.nastepniki(n, tab, nast)
+        start = time.time()
+        DFS_nastepniki(n, nast, 0)
+        wynik = time.time() - start
+        plik.write("dfs_nastepniki;{};{}\n".format(i, wynik))
+        print(dfs)
+        dfs = []
+        kraw = []
+        krawedzi.krawedzi(n, tab, kraw)
+        start = time.time()
+        DFS_krawedzie(n, kraw, 0)
+        wynik = time.time() - start
+        plik.write("dfs_krawedzie;{};{}\n".format(i, wynik))
+        print(dfs)
+    plik.close()
