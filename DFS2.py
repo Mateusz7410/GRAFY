a80eeba0574ffcfa36ef6e68589f1b2767989e1d
Root <pankernel@tutanota.com>
2016-05-09 21:23:17 +0200

    General changes, DFS revision 2. TODO: pre-order and postorder.

---
+++ DFS2.py
@@ -0,0 +1,97 @@
+import nastepniki
+import krawedzi
+import generowanie
+import macierz
+import time
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+def DFS_s(n, odw, tab, v1):
+    if odw[v1] == False:
+        odw[v1] = True
+        for i in range(n):
+            if tab[v1][i] == 1 and odw[i] == False:
+                DFS_s(n, odw, tab, i)
+    print(v1)
+
+
+def DFS_sasiedztwa(n, tab, start):
+    v1 = start
+    odwiedzone = [False] * n
+    while False in odwiedzone:
+        DFS_s(n, odwiedzone, tab, v1)
+        if False in odwiedzone:
+            v1 = odwiedzone.index(False)
+
+def DFS_n(n, odw, nast, v1):
+    if odw[v1] == False:
+        odw[v1] = True
+        for i in nast[v1]:
+            if  odw[i] == False:
+                DFS_n(n, odw, nast, i)
+    print(v1)
+
+def DFS_nastepniki(n, nast, start):
+    v1 = start
+    odwiedzone = [False] * n
+    while False in odwiedzone:
+        DFS_n(n, odwiedzone, nast, v1)
+        if False in odwiedzone:
+            v1 = odwiedzone.index(False)
+
+def DFS_k(n, odw, kraw, v1):
+    if odw[v1] == False:
+        odw[v1] = True
+        for i in range(n):
+            if kraw[i][0] == v1 and odw[kraw[i][1]] == False:
+                DFS_k(n, odw, kraw, kraw[i][1])
+    print(v1)
+
+
+
+def DFS_krawedzie(n, kraw, start):
+    v1 = start
+    odwiedzone = [False] * (n-1)
+    while False in odwiedzone:
+        DFS_k(n, odwiedzone, kraw, v1)
+        if False in odwiedzone:
+            v1 = odwiedzone.index(False)
+
+
+
+
+
+if __name__ == "__main__":
+    tab = [[0,1,0,0,1,0,0,0], [0,0,0,0,0,0,1,0], [0,0,0,0,1,1,0,0], [0,0,0,0,0,1,0,1], [0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,1], [0,0,0,0,0,0,0,0]]
+    #tab =[]
+    #n = 1000
+    #generowanie.generate(n, tab)
+
+    DFS_sasiedztwa(len(tab), tab, 0)
+    nast = []
+    nastepniki.nastepniki(len(tab), tab, nast)
+    DFS_nastepniki(len(nast), nast,0)
+    kraw = []
+    dfs = []
+    krawedzi.krawedzi(len(tab), tab, kraw)
+    DFS_krawedzie(len(kraw), kraw, 0)
+
